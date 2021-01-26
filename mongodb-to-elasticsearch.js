48991e9a70d5bc29ea061b5daedc6a18e845c6f2
ilanserman <67495285+ilanserman@users.noreply.github.com>
2021-01-26 09:31:11 -0300

    Update mongodb-to-elasticsearch.js

--- mongodb-to-elasticsearch.js
+++ mongodb-to-elasticsearch.js
@@ -27,7 +27,7 @@ async function init() {
     console.log("MongoDB ElasticSearch sync: Connected MongoDB server")
 
     //Wait for changes with operations insert, update or replace and update ES database
-    async function getUpsertChangeStream() {
+    async function detectInsertUpdateReplaceChangeStream() {
       const changeStream = (await db.collection('products')).watch([
         {
           "$match": {
@@ -45,7 +45,7 @@ async function init() {
       return changeStream;
     }
 
-    async function getDeleteChangeStream() {
+    async function detectDeleteChangeStream() {
       const changeStream = (await db.collection('products')).watch([
         {
           "$match": {
@@ -63,29 +63,29 @@ async function init() {
       return changeStream;
     }
 
-    const upsertChangeStream = await getUpsertChangeStream();
-    upsertChangeStream.on("change", async change => {
-      console.log("Pushing document to Elasticsearch, id:", change.fullDocument._id);
-      change.fullDocument.id = change.fullDocument._id;
-      Reflect.deleteProperty(change.fullDocument, "_id");
+    const insertUpdateReplaceChangeStream = await detectInsertUpdateReplaceChangeStream();
+    upsertChangeStream.on("change", async next => {
+      console.log("Pushing document to Elasticsearch, id:", next.fullDocument._id);
+      next.fullDocument.id = next.fullDocument._id;
+      Reflect.deleteProperty(next.fullDocument, "_id");
       const response = await esclient.index({
-        "id": change.fullDocument.id,
+        "id": next.fullDocument.id,
         "index": "YOUR_DESIRED_ELASTICSEARCH_INDEX",
-        "body": change.fullDocument,
+        "body": next.fullDocument,
         "type": "doc"
       });
       console.log("Document upserted, status code:", response.statusCode);
     });
-    upsertChangeStream.on("error", error => {
+    insertUpdateReplaceChangeStream.on("error", error => {
       console.error(error);
     });
 
 
-    const deleteChangeStream = await getDeleteChangeStream();
-    deleteChangeStream.on("change", async change => {
-      console.log("Deleting data from ElasticSearch, id", change.documentKey._id);
+    const deleteChangeStream = await detectDeleteChangeStream();
+    deleteChangeStream.on("change", async next => {
+      console.log("Deleting data from ElasticSearch, id", next.documentKey._id);
       const response = await esclient.delete({
-        "id": change.documentKey._id,
+        "id": next.documentKey._id,
         "index": "YOUR_DESIRED_ELASTICSEARCH_INDEX",
         "type": "doc"
       });
